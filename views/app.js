4f643a9b767596299ba46d063c3d2ad9a996fcb1
karitotp <karito@mapbox.com>
2016-06-27 06:30:09 -0500

    add filter marker

--- views/app.js
+++ views/app.js
@@ -1,7 +1,13 @@
-
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2FyaXRvdHAiLCJhIjoib0ppeVhnWSJ9.JOLM9BQLqtI_bjvNzjNPew';
 var geolocate = document.getElementById('geolocate');
 var map = L.mapbox.map('map', 'mapbox.streets');
+
+
+hotel = document.getElementById('filter-hotel'),
+restaurant = document.getElementById('filter-restaurant'),
+all = document.getElementById('filter-all');
+
+
 map.attributionControl.setPosition('bottomleft');
 
 // create the initial directions object, from which the layer
@@ -26,15 +32,18 @@ var directionsInstructionsControl = L.mapbox.directions.instructionsControl('ins
 
 var myLayer = L.mapbox.featureLayer().addTo(map);
 
+
+
 // This uses the HTML5 geolocation API, which is available on
 // most mobile browsers and modern browsers, but not in Internet Explorer
 //
 // See this chart of compatibility for details:
 // http://caniuse.com/#feat=geolocation
+
 if (!navigator.geolocation) {
     geolocate.innerHTML = 'Geolocation is not available';
 } else {
-    geolocate.onclick = function (e) {
+    geolocate.onclick = function(e) {
         e.preventDefault();
         e.stopPropagation();
         map.locate();
@@ -60,6 +69,7 @@ map.on('locationfound', function(e) {
     });
 
     // And hide the geolocation button
+    myLayer.openPopup();
     geolocate.parentNode.removeChild(geolocate);
 });
 
@@ -68,3 +78,55 @@ map.on('locationfound', function(e) {
 map.on('locationerror', function() {
     geolocate.innerHTML = 'Position could not be found';
 });
+
+
+for (var i = 1; i < dato.features.lenght; i++) {
+ dato.features[i].properties["title"]= dato.features[i].properties.name;
+
+    if(dato.features[i].properties.amenity=="restaurant"){
+    dato.features[i].properties["marker-symbol"]="restaurant";
+      
+    }
+    if(dato.features[i].properties.tourism=="hotel"){
+    dato.features[i].properties["marker-symbol"]="lodging";
+    
+    }
+
+    };
+
+
+var markers = L.mapbox.featureLayer()
+    .setGeoJSON(dato)
+    .addTo(map);
+
+     hotel.onclick = function(e) {
+        all.className = '';
+        restaurant.className = '';
+        this.className = 'active';
+        markers.setFilter(function(f) {
+            return f.properties['marker-symbol'] === 'lodging';
+        });
+        return false;
+    };
+
+    restaurant.onclick = function(e) {
+        all.className = '';
+        hotel.className = '';
+        none.className = '';
+        this.className = 'active';
+        markers.setFilter(function(f) {
+            return f.properties['marker-symbol'] === 'restaurant';
+        });
+        return false;
+    };
+
+     all.onclick = function() {
+        hotel.className = '';
+        restaurant.className = '';
+        this.className = 'active';
+        map.featureLayer.setFilter(function(f) {
+            // Returning true for all markers shows everything.
+            return true;
+        });
+        return false;
+    };
